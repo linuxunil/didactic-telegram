a747a0733f722679b991a8f51c5cedc223aee292
Disco <linuxunil@gmail.com>
2025-12-28 12:53:50 -0600

    Add deps

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,17 +9,28 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-#dnf5 install -y zellij
+# remove kde plasma
+dnf5 -y remove plasma-workspace plasma-* kde-*
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-dnf5 -y copr enable avengemedia/dms
-dnf5 -y install niri dms
-#### Example for enabling a System Unit File
+# setup niri
+dnf5 -y install					\
+	niri						\
+	alacritty					\
+	gdm							\
+	xdg-desktop-portal-gtk		\
+	xdg-desktop-portal-gnome	\
+	gnome-keyring				\
+	nautilus					\
+	mako						\
+	fuzzel						\
+	waybar						\
+	swayidle					\
+	swaylock					\
+	polkit-kde					\
+	xwayland-satellite			\
+	swaybg
 
 systemctl enable podman.socket
+systemctl --global add-wants niri.service mako.service
+systemctl --global add-wants niri.service swayidle.service
+systemctl --global add-wants niri.service plasma-polkit-agent.service#!/bin/bash
