fd02a07ee92e184664403ac2731c54b8a91e4ebc
Disco <linuxunil@gmail.com>
2025-12-28 12:08:24 -0600

    wqAdd zellij, niri, dms

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y zellij
 
 # Use a COPR Example:
 #
@@ -18,7 +18,9 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y install niri dms
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable niri.service dms
